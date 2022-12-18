86a7f2f4e2f599e63a1c41ebff311aa9eda8e97f
Usama Blavins <usama.blavins1@gmail.com>
2022-12-18 14:06:54 +0000

    Enable dark theme button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import CssBaseline from '@mui/material/CssBaseline';
 import NavigationBar from 'components/common/NavigationBar';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
+import { useState } from 'react';
+
+interface AppState {
+    isDark: boolean;
+}
 
 const App = () => {
+    const [appState, setAppState] = useState<AppState>({ isDark: true });
+
+    const setTheme = () => {
+        return createTheme({
+            palette: {
+                mode: appState.isDark ? 'dark' : 'light',
+            },
+        });
+    };
+
     return (
-        <div>
+        <ThemeProvider theme={setTheme()}>
+            <CssBaseline />
             <BrowserRouter>
-                <NavigationBar />
+                <NavigationBar
+                    isDark={appState.isDark}
+                    themeToggle={() => setAppState({ ...appState, isDark: !appState.isDark })}
+                />
             </BrowserRouter>
-        </div>
+        </ThemeProvider>
     );
 };
 
--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -1,9 +1,16 @@
+import { Container } from 'react-bootstrap';
 import '../styles/Background.scss';
 
+import { Brightness4, Brightness7 } from '@mui/icons-material';
 import Navbar from 'react-bootstrap/Navbar';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-const NavigationBar = (_props: any) => {
+interface NavigationBarProps {
+    isDark: boolean;
+    themeToggle: () => void;
+}
+
+const NavigationBar = (props: NavigationBarProps) => {
     const location = useLocation();
     const navigate = useNavigate();
     console.log(location);
@@ -11,8 +18,12 @@ const NavigationBar = (_props: any) => {
         <span className='header'>
             <Navbar bg='nav-bar' variant='dark' expand='lg' sticky='top'>
                 <Navbar.Brand data-testid='navbar-brand' className='navbar-brand' onClick={() => navigate('/')}>
-                    <span>CLOUD PROVISIONER</span>
+                    <span>Cloud Provisioner</span>
                 </Navbar.Brand>
+                <Navbar.Toggle />
+                <Container onClick={props.themeToggle} className='justify-content-end'>
+                    {props.isDark ? <Brightness4 /> : <Brightness7 />}
+                </Container>
             </Navbar>
         </span>
     );
